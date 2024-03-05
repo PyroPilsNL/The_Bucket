baaf5b12d15ac582e2a7e741f601810435d2101d
wouterwolf <w.wolf7292@gmail.com>
2024-03-05 13:46:37 +0100

    Omdat het moest

--- The_Bucket_Tests/obj/Debug/net8.0/The_Bucket_Tests.AssemblyInfo.cs
+++ The_Bucket_Tests/obj/Debug/net8.0/The_Bucket_Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbe8c5a946213c09505af7e2f21848315a552d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
