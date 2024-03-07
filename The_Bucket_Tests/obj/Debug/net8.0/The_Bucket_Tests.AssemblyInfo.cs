20cc56b03d83b6d935074400ab4c18e76980ed45
wouterwolf <w.wolf7292@gmail.com>
2024-03-07 12:13:09 +0100

    Test

--- The_Bucket_Tests/obj/Debug/net8.0/The_Bucket_Tests.AssemblyInfo.cs
+++ The_Bucket_Tests/obj/Debug/net8.0/The_Bucket_Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172a44067c0e006866231de8650292359e7431de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Bucket_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
